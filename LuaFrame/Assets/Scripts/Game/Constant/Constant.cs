053968df8d579fe0684aad3ddab4bd9f4f93c4d4
HZJ1 <979523815@qq.com>
2018-05-29 19:43:00 +0800

    添加文件

---
+++ LuaFrame/Assets/Scripts/Game/Constant/Constant.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Constant : MonoBehaviour {
+    //读取资源的模式 true 表示读取bundle
+    public static bool readModel = false;
+    //平台
+    public static string platform = "Windows";
+    //资源根目录
+    public static string resPath = "";
+    //tolua 自带的lua路径
+    public static string luaPath = "";
+    //资源bundle 路径
+    public static string abPath = "";
+     
+	
+}
---
+++ LuaFrame/Assets/Scripts/Game/Manager/AppManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    void Start () {
+        Initialize();
+
+    }
+    /// <summary>
+    /// 初始化平台
+    /// </summary>
+    void Initialize()
+    {
+        //初始化平台
+        InitializePlatform();
+        //初始化资源相关路径
+        InitializePath();
+        //初始化相关资源
+        InitilaizeRes();
+    }
+
+    #region 初始化平台
+    /// <summary>
+    /// 初始化平台
+    /// </summary>
+    public void InitializePlatform()
+    {
+#if WINDOWS_BUILD || UNITY_STANDALONE
+        Constant.platform = "Windows";
+#elif UNITY_ANDROID
+        Constant.platform = "Android";
+#elif UNITY_IOS
+        Constant.platform = "iOS";
+#endif
+
+    }
+    #endregion
+    #region 初始化不同平台的相关路径
+    void InitializePath()
+    {
+#if UNITY_STANDALONE || UNITY_EDITOR
+        Constant.resPath= Application.dataPath + "/../../GameRes/Lua";
+        Constant.luaPath = Constant.resPath + "/Lua";
+        Constant.abPath =  Constant.resPath + "/AssetBundles/Windows/";
+#endif
+    }
+    #endregion
+    #region 初始化相关资源
+    void InitilaizeRes()
+    {
+        CheckNeedUpdate();
+
+    }
+    #endregion
+
+    /// <summary>
+    /// 检测是否要更新
+    /// </summary>
+    public void CheckNeedUpdate()
+    {
+    }
+}
